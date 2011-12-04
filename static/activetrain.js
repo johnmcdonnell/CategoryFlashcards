0c9bc9e0e828e65a85e7b7fd141c97cada10c1d7
John McDonnell <john.v.mcdonnell@gmail.com>
2011-12-04 18:16:27 -0500

    Trying to use namespaces, it's been interesting.

--- static/activetrain.js
+++ static/activetrain.js
@@ -1,7 +1,43 @@
 
+/********************
+// Domain general code
+********************/
+// Helper functions
+
+// Fisher-Yates shuffle algorithm.
+// modified from http://sedition.com/perl/javascript-fy.html
+function shuffle ( myArray ) {
+  if ( ! myArray.length ) { return false; }
+  for ( var i=myArray.length; i>0; i-- ) {
+     var j = Math.floor( Math.random() * ( i + 1 ) );
+     var tempi = myArray[i];
+     var tempj = myArray[j];
+     myArray[i] = tempj;
+     myArray[j] = tempi;
+   }
+  return true;
+}
+
+// AJAX post function.
+postback = function(destination, tosend) {
+	$.ajax("submit.py", {
+		type: "POST",
+		data: tosend,
+		error: function(jqXHR,textStatus,errorThrown) { setTimeout( $.ajax(this), 1000 ); }
+	});
+};
+
+// Mean of booleans (true==1; false==0)
+boolmean = function(arr) {
+	count = 0;
+	for (i=0; i<arr.length; i++) {
+		if (arr[i]) { count++; } 
+	}
+	return 100* count / arr.length;
+};
 
 /********************
-// CODE FOR TRAINING
+// Task-specific code
 ********************/
 
 // Globals defined initially.
@@ -14,99 +50,24 @@ cardnames = [
 	"images/STIM05.GIF",
 	"images/STIM06.GIF",
 	"images/STIM07.GIF"];
-var sampleunits = 16;
 var cardh = 180, cardw = 140, upper = 0, left = 0;
 var subjid = 0;
-
-// Globals whose values will be filled in on window.onload.
-var cards = new Array();
-var timerects;
-
-
-// Mutable Globals
-var lock = false;
-
-// Dict storing all the stuff we'll return
-var ret = {
-	subjid: subjid,
-	queries: [],
-	hits: []
-};
-
-
-// Helper functions
-
-// Fisher-Yates shuffle algorithm 
-// modified from http://sedition.com/perl/javascript-fy.html
-function shuffle ( myArray ) {
-  if ( ! myArray.length ) { return false; }
-  for ( var i=myArray.length; i>0; i-- ) {
-     var j = Math.floor( Math.random() * ( i + 1 ) );
-     var tempi = myArray[i];
-     var tempj = myArray[j];
-     myArray[i] = tempj;
-     myArray[j] = tempi;
-   }
-  return myArray;
-}
-
-boolmean = function(arr) {
-	count = 0;
-	for (i=0; i<arr.length; i++) {
-		if (arr[i]) { count++; } 
-	}
-	return 100* count / arr.length;
-};
-
-// Interface functions
-cardclick = function (cardid) {
-	return function() {
-		if ( ! timerects.length ) { return false; }
-		if ( lock ) {  return false; }
-		lock = true;
-		cards[cardid][2].show();
-		setTimeout(
-			function(){
-				cards[cardid][2].hide();
-				timerects.pop().hide();
-				if ( ! timerects.length ) {
-					alert("You have finished! Click OK to go on to the test phase.");
-					initializeTest();
-				}
-				lock = false;
-			},
-			500);
-		return true;
-	};
-};
+var taskobject;
 
 // Task-specific functions
 catfun = function ( num ) {
 	return num % 2;
 };
 
+/********************
+// CODE FOR TRAINING
+********************/
+
+
+// Interface functions
+
 initializeTraining = function () {
-	var cardh = 180, cardw = 150;
-	var imgh = 100, imgw = 100;
-	var ncardswide = 4;
-	var ncardstall = 2;
-	var upper = 0;
-	var left = 0;
-	// Canvas for the cards.
-	var nowX, nowY, w = ncardswide*cardw, h = ncardstall*cardh, r=30;
-	var cardpaper = Raphael(document.getElementById("cardcanvas"), w, h);
-	// Canvas for the timer.
-	var timertotalw = w/2;
-	var timertotalh = 50;
-	var w2 = timertotalw, h2 = timertotalh;
-	var timerpaper = Raphael(document.getElementById("timercanvas"), w2, h2);
 	
-	// Category labels are just the letters.
-	var categorynames= [ "A", "B" ];
-	
-	var randomcardplace = new Array();
-	for ( i=0; i < cardnames.length; i ++ ){ randomcardplace.push( i ); }
-	randomcardplace = shuffle( randomcardplace );
 	
 	for ( i=0; i < cardnames.length; i ++){
 		var cardloci = randomcardplace[i];
@@ -123,7 +84,39 @@ initializeTraining = function () {
 		cards[i].click( cardclick(i) );
 	}
 	
-	timerects = timerpaper.set();
+};
+
+var TrainingPhase = function() {
+	var i; // just initializing the iterator dummy
+	
+	// Globals defined initially.
+	var sampleunits = 16;
+	
+	// Mutable Globals
+	var lock = false;
+	var cards = new Array();
+	
+	this.ret = {
+		searchchoices: []
+	};
+	
+	// View variables
+	var cardh = 180, cardw = 150;
+	var imgh = 100, imgw = 100;
+	var ncardswide = 4, ncardstall = 2;
+	var upper = 0, left = 0;
+	
+	// Canvas for the cards.
+	var nowX, nowY, w = ncardswide*cardw, h = ncardstall*cardh, r=30;
+	var cardpaper = Raphael(document.getElementById("cardcanvas"), w, h);
+	
+	// Canvas for the timer.
+	var timertotalw = w/2;
+	var timertotalh = 50;
+	var w2 = timertotalw, h2 = timertotalh;
+	var timerpaper = Raphael(document.getElementById("timercanvas"), w2, h2);
+	
+	var timerects = timerpaper.set();
 	timerectw = timertotalw / (sampleunits*2-1);
 	for ( i=0; i < sampleunits; i ++){
 		timerects.push( 
@@ -132,8 +125,67 @@ initializeTraining = function () {
 				0,
 				timerectw, timertotalh, [5]).attr({fill:"red" }));
 	}
+	
+	// Category labels are just the letters.
+	var categorynames= [ "A", "B" ];
+	
+	// Card locations are randomized.
+	var randomcardplace = new Array();
+	for ( i=0; i < cardnames.length; i ++ ){ 
+		randomcardplace.push( i ); 
+	}
+	shuffle( randomcardplace );
+	
+	this.cardclick = function (cardid) {
+		return function() {
+			if ( ! timerects.length ) { return false; }
+			if ( lock ) {  return false; }
+			lock = true;
+			cards[cardid][2].show();
+			timestamp = new Date().getTime();
+			this.ret.searchchoices.push( { card:cardid, time: timestamp } );
+			setTimeout(
+				function(){
+					cards[cardid][2].hide();
+					timerects.pop().hide();
+					if ( ! timerects.length ) {
+						// alert( this.ret.searchchoices );
+						alert("You have finished! Click OK to go on to the test phase.");
+						initializeTest();
+					}
+					lock = false;
+				},
+				500);
+			return true;
+		};
+	};
+	
+	for ( i=0; i < cardnames.length; i ++){
+		var cardloci = randomcardplace[i];
+		cards[i] = cardpaper.set();
+		var thisleft = cardw * (cardloci % 4) + left;
+		var thistop = cardh*Math.floor(cardloci/4) + upper;
+		var imgoffset = (cardw-imgw)/2;
+		
+		cards[i].catnum = catfun( i );
+		cards[i].push( cardpaper.rect( thisleft + (imgoffset/2), thistop+(imgoffset/2), imgw+(imgoffset), cardh-imgoffset).attr({fill:"black" }));
+		cards[i].push( cardpaper.image( cardnames[i], thisleft + imgoffset, thistop+imgoffset, imgw, imgh) );
+		cards[i].push( cardpaper.text( thisleft + cardw/2, (thistop+imgoffset + thistop+(imgoffset/2) + cardh-imgoffset + imgh)/2, categorynames[cards[i].catnum] ).attr({ fill: "white", "font-size":36 }).hide() );
+		
+		cards[i].click( this.cardclick(i) );
+	}
+	
+	// Now for the public methods
+	return {
+		getresps: function() {
+			return this.ret;
+		}
+	};
 };
 
+$(document).ready( function(){
+	taskobject = TrainingPhase();
+});
 
 /********************
 // CODE FOR TEST
@@ -204,13 +256,6 @@ initializeTest = function () {
 // Postback
 ********************/
 
-postback = function() {
-	$.ajax("submit.py", {
-		type: "POST",
-		data: ret,
-		error: function(jqXHR,textStatus,errorThrown) { setTimeout( $.ajax(this), 1000 ); }
-	});
-};
 
 
 
